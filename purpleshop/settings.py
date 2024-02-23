757594928a6977d74b285d1b8b369ec97f23a4ac
Femina Azeez <feminaazeez.456@gmail.com>
2024-02-23 12:08:35 +0530

    uncommented sttic

--- purpleshop/settings.py
+++ purpleshop/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 
 
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 
