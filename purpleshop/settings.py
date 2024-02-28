484849e1fd61b9a5ba45cd56f76e3438fa712ac1
Femina Azeez <feminaazeez.456@gmail.com>
2024-02-28 10:09:23 +0530

    The file path in the static is changed

--- purpleshop/settings.py
+++ purpleshop/settings.py
@@ -154,12 +154,15 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
 
 
 
