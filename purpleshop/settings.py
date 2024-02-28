19a52d7c78e04524daafcf62ff33359e0662ed5e
Femina Azeez <feminaazeez.456@gmail.com>
2024-02-28 10:48:42 +0530

    The static files dir name is changed'

--- purpleshop/settings.py
+++ purpleshop/settings.py
@@ -159,7 +159,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # ]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles/'),
 ]
 
 # # media files configurations
