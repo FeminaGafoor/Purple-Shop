6f2906d3a3dc1eab14086d2efd0273b609ce811b
Femina Azeez <feminaazeez.456@gmail.com>
2024-02-28 11:17:26 +0530

    updated

--- purpleshop/settings.py
+++ purpleshop/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
 
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
